474019b831bcfbdc0fcc65ea550bdbbbfa744a5e
Florent Aden <f.aden@gns.cri.nz>
2022-10-05 14:36:59 +1300

    fits observation datasource

---
+++ grafana-plugins/fits-observation/src/datasource.ts
@@ -0,0 +1,105 @@
+
+import {
+  DataQueryRequest,
+  DataQueryResponse,
+  DataSourceApi,
+  DataSourceInstanceSettings,
+  MutableDataFrame,
+  FieldType,
+  // dateTime,
+} from '@grafana/data';
+
+import { getBackendSrv } from '@grafana/runtime';
+
+
+import { MyQuery, MyDataSourceOptions } from './types';
+
+const fitsDataApi = 'http://fits.geonet.org.nz/observation?';
+
+export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
+  constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
+    super(instanceSettings);
+  }
+
+  async doRequest(query: MyQuery, from: number, to: number) {
+    var days_float: number;
+    var days: number;
+    
+    days_float = (to - from) / 3600 / 24 / 1000;
+    if (days_float < 1) {
+      days_float = 1;
+    }
+
+    days = days_float | 0;
+
+    const response = await getBackendSrv().datasourceRequest({
+      method: 'GET',
+      url:
+        fitsDataApi +
+        'typeID=' +
+        query.type +
+        '&siteID=' +
+        query.site +
+        '&days='   +
+        days,
+    });
+
+    // let fitsResp = response.data.split("\n") as FitsDataResponse[];
+    // return fitsResp[0].data;
+    return response.data
+  }
+
+  async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
+    const { range } = options;
+    const from = range!.from.valueOf();
+    const to = range!.to.valueOf();
+    // Return a constant for each query.
+    const promises = options.targets.map((query) =>
+      this.doRequest(query, from, to).then((response) => {
+        var resp_split = response.split("\n");
+        var headers = resp_split[0].split(",");
+
+        const frame = new MutableDataFrame({
+          refId: query.refId,
+          name: query.site,
+          fields: [
+            { name: headers[0], type: FieldType.time },
+            { name: headers[1], type: FieldType.number },
+            { name: headers[2], type: FieldType.number },
+          ],
+        });
+        resp_split.forEach((line: any, index: number) => {
+          if (index > 0) {
+            var point = line.split(",");
+
+            var t: Date;
+            var value: number;
+            var error: number;
+
+            t = point[0];
+            value = point[1];
+            error = point[2];
+            
+            if (isNaN(value) || value == null) {
+              console.log("found a NaN or null here!");
+            } else {
+              frame.appendRow([t, value, error]);
+            }
+          }
+        });
+
+        return frame;
+      })
+    );
+
+    return Promise.all(promises).then((data) => ({ data }));
+  }
+
+  async testDatasource() {
+    // Implement a health check for your data source.
+    return {
+      status: 'success',
+      message: 'Success',
+    };
+  }
+}
